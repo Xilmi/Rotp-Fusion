23d4d730c5b0ce8864c2c49270cf8d56ac384717
BrokenRegistry <Broken.Registry@protonmail.com>
2022-09-25 23:03:25 -0400

    Added new param to offer all options

---
+++ src/rotp/ui/util/ParamObject.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2015-2020 Ray Fowler
+ *
+ * Licensed under the GNU General Public License, Version 3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package rotp.ui.util;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+
+import rotp.model.game.MOO1GameOptions;
+
+public class ParamObject extends AbstractParam<Object> {
+	
+
+	// ===== Constructors =====
+	//
+	/**
+	 * @param gui  The label header
+	 * @param name The name
+	 * @param defaultValue The default value
+	 */
+	public ParamObject(String gui, String name, String defaultValue) {
+		super(gui, name, defaultValue);
+	}
+	// ===== Overriders =====
+	//
+	@Override public void setFromCfgValue(String newValue)	{ value(newValue); }	
+	@Override public void prev() {}
+	@Override public void next() {}
+	@Override public void toggle(MouseWheelEvent e)	{}
+	@Override public void toggle(MouseEvent e) {}
+	@Override public void setFromOptions(MOO1GameOptions options) {
+		set(options.getObjectOptions(labelId(), defaultValue()));
+	}
+	@Override public void setOptions(MOO1GameOptions options) {
+		options.setObjectOptions(labelId(), get());
+	}
+	// ===== Other Methods =====
+	//
+}
