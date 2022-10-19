8106f21fc348622e8adc79b33d9df3f3b85a4cfc
Xilmi <ail.st@gmx.de>
2022-10-19 14:54:42 +0200

    Fusion is default-AI again

--- src/rotp/model/game/MOO1GameOptions.java
+++ src/rotp/model/game/MOO1GameOptions.java
@@ -1090,9 +1090,9 @@ public class MOO1GameOptions implements Base, IGameOptions, Serializable {
         selectedNumberOpponents = defaultOpponentsOptions();
         selectedPlayerRace(random(startingRaceOptions()));
         selectedGameDifficulty = DIFFICULTY_NORMAL;
-        selectedOpponentAIOption = OPPONENT_AI_PERSONALITY;
+        selectedOpponentAIOption = OPPONENT_AI_CRUEL;
         for (int i=0;i<specificOpponentAIOption.length;i++)
-            specificOpponentAIOption[i] = OPPONENT_AI_PERSONALITY;
+            specificOpponentAIOption[i] = OPPONENT_AI_CRUEL;
         setToDefault();
         generateGalaxy();
     }
