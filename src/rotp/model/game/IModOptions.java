6ca4b665bc1c975eef3cdba1b86dad6107cbab62
dHannasch <David.A.Hannasch@gmail.com>
2023-07-02 09:55:14 -0600

    rename to trackUFOsAcrossTurns

--- src/rotp/model/game/IModOptions.java
+++ src/rotp/model/game/IModOptions.java
@@ -133,7 +133,7 @@ public interface IModOptions extends IFlagOptions, IPreGameOptions, IInGameOptio
 				headerSpacer,
 				new ParamTitle("GAME_OTHER"),
 				showAlliancesGNN, showLimitedWarnings,
-				techExchangeAutoRefuse, autoTerraformEnding, activateBasicScanner, autoplay
+				techExchangeAutoRefuse, autoTerraformEnding, trackUFOsAcrossTurns, autoplay
 				)));
 		map.add(new LinkedList<>(Arrays.asList(
 				new ParamTitle("GAME_RELATIONS"),
