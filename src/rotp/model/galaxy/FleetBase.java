9a8112630faa86035878f3bc0bcfefecdb013a40
dHannasch <David.A.Hannasch@gmail.com>
2023-07-08 12:11:19 -0600

    FleetBase.arrivalTime

--- src/rotp/model/galaxy/FleetBase.java
+++ src/rotp/model/galaxy/FleetBase.java
@@ -2,4 +2,7 @@ package rotp.model.galaxy;
 
 public abstract class FleetBase {
   private static final long serialVersionUID = 1L;
+  private float arrivalTime = Float.MAX_VALUE;
+
+  public float arrivalTime() { return arrivalTime; }
 }
