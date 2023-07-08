43637bbb9555f69ad9f0adace10594bc65a620eb
dHannasch <David.A.Hannasch@gmail.com>
2023-07-08 12:44:08 -0600

    Move boolean displayed up to FleetBase

--- src/rotp/model/galaxy/FleetBase.java
+++ src/rotp/model/galaxy/FleetBase.java
@@ -4,9 +4,17 @@ public abstract class FleetBase {
   private static final long serialVersionUID = 1L;
   private float arrivalTime = Float.MAX_VALUE;
 
+  private transient boolean displayed;
+
   public float arrivalTime() { return arrivalTime; }
-  abstract float calculateArrivalTime();
+  protected abstract float calculateArrivalTime();
   public void setArrivalTime() {
     arrivalTime = calculateArrivalTime();
+  }
+
+  public boolean displayed() { return displayed; }
+  protected abstract boolean decideWhetherDisplayed();
+  public void setDisplayed() {
+    displayed = decideWhetherDisplayed();
   }
 }
