549bdeac4d0b32c9fd4c6052f648c0f60d7c95d8
dHannasch <David.A.Hannasch@gmail.com>
2023-07-08 12:54:15 -0600

    decideWhetherDisplayed takes GalaxyMapPanel

--- src/rotp/model/galaxy/FleetBase.java
+++ src/rotp/model/galaxy/FleetBase.java
@@ -1,5 +1,7 @@
 package rotp.model.galaxy;
 
+import rotp.ui.main.GalaxyMapPanel;
+
 public abstract class FleetBase {
   private static final long serialVersionUID = 1L;
   private float arrivalTime = Float.MAX_VALUE;
@@ -13,7 +15,7 @@ public abstract class FleetBase {
   }
 
   public boolean displayed() { return displayed; }
-  protected abstract boolean decideWhetherDisplayed();
+  protected abstract boolean decideWhetherDisplayed(GalaxyMapPanel map);
   public void setDisplayed() {
     displayed = decideWhetherDisplayed();
   }
